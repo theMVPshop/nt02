d2c8dd04e4a53f91c9cca65e0d03422ee8e9b5dd
michaelscoggins <rhythmghosts@gmail.com>
2021-04-27 18:53:48 -0500

    might work but getting error code 429

--- studentProfileScraper.js
+++ studentProfileScraper.js
@@ -17,9 +17,6 @@ async function closeBrowser(browser) {
   return browser.close();
 }
 
-// console.log("links obj", Object.entries(studentLinks));
-console.log("links obj", Object.values(studentLinks)[1]);
-
 async function playTest(url) {
   const { browser, page } = await startBrowser();
   page.setViewport({ width: 1366, height: 768 });
@@ -31,72 +28,76 @@ async function playTest(url) {
   await page.click(CTA_SELECTOR);
   await page.waitForNavigation();
   let totalArr = [];
-
-  for (i = 1; i <= 5; i++) {
+  let len = Object.values(studentLinks).length;
+  for (i = 1; i <= len; i++) {
     await page.goto(Object.values(studentLinks)[i]);
-    // await page.waitForNavigation();
-    // await page.waitForSelector(`body`);
-    // console.log(page);
-    // await page.waitForSelector(
-    //   `#main > div > div > section > div:nth-of-type(2) > div > div > div > div > img`
-    // );
-    // await page.waitForSelector(
-    //   `#experience-section > ul > li > section > div > div > a > div`
-    // );
-    // await page.waitForSelector(
-    //   `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div:nth-of-type(2) > h3`
-    // );
+    console.log("link", Object.values(studentLinks)[i]);
     await page.waitForSelector(
-      `#experience-section > ul > li > section > div > div > a > div > img`
+      `#main > div > div > section > div:nth-of-type(2) > div > div > div > div > img`
     );
-    // await page.waitForSelector(
-    //   `#experience-section > ul > li:nth-child(2) > section > div > a > div > div > img`
-    // );
+    setTimeout(function () {}, 12000);
 
     let result = await page.evaluate(() => {
       let arr = [];
+
       const PHOTOSELECTOR = `#main > div > div > section > div:nth-of-type(2) > div > div > div > div > img`;
-      const COMPANY1NAMEROLESELECTOR = `#experience-section > ul > li > section > div > div > a > div`;
-      const COMPANY2NAMESELECTOR = `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div > img`;
-      const COMPANY2ROLESELECTOR = `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div:nth-of-type(2) h3`;
+
+      const COMPANY1NAMESELECTOR = `#experience-section > ul > li > section > div > div > a > div > p:nth-of-type(2)`;
+      const COMPANY1ROLESELECTOR = `#experience-section > ul > li > section > div > div > a > div > h3`;
+
+      const COMPANY2NAMESELECTOR = `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div > p:nth-of-type(2)`;
+      const COMPANY2ROLESELECTOR = `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div > h3`;
+
       const COMPANY1LOGOSELECTOR = `#experience-section > ul > li > section > div > div > a > div > img`;
+
       const COMPANY2LOGOSELECTOR = `#experience-section > ul > li:nth-of-type(2) > section > div > div > a > div > img`;
 
       let photo = document.querySelector(PHOTOSELECTOR).src;
 
-      let company1Name = document
-        .querySelectorAll(COMPANY1NAMEROLESELECTOR)[1]
-        .innerText.split("\n")[4];
+      let company1Name =
+        (document.querySelector("#experience-section") &&
+          document.querySelector(COMPANY1NAMESELECTOR).innerText) ||
+        null;
 
-      let company1Role = document
-        .querySelectorAll(COMPANY1NAMEROLESELECTOR)[1]
-        .innerText.split("\n")[0];
+      let company1Role =
+        (document.querySelector("#experience-section") &&
+          document.querySelector(COMPANY1ROLESELECTOR).innerText) ||
+        null;
 
-      let company2name = document.querySelector(COMPANY2NAMESELECTOR).alt;
+      let company2name =
+        (document.querySelector(COMPANY2NAMESELECTOR) &&
+          document.querySelector(COMPANY2NAMESELECTOR).innerText) ||
+        null;
 
-      let company2role = document.querySelector(COMPANY2ROLESELECTOR).innerText;
+      let company2role =
+        (document.querySelector(COMPANY2ROLESELECTOR) &&
+          document.querySelector(COMPANY2ROLESELECTOR).innerText) ||
+        null;
 
-      let company1logo = document.querySelectorAll(COMPANY1LOGOSELECTOR)[0]
-        .currentSrc;
+      let company1logo =
+        (document.querySelector("#experience-section") &&
+          document.querySelector(COMPANY1LOGOSELECTOR).currentSrc) ||
+        null;
 
       let company2logo =
-        document.querySelectorAll(COMPANY2LOGOSELECTOR)[0].src || null;
+        (document.querySelector(COMPANY2LOGOSELECTOR) &&
+          document.querySelector(COMPANY2LOGOSELECTOR).src) ||
+        null;
 
       arr.push(
-        `Photo URL: ${photo}\nCompany 1 Name: ${company1Name}\nCompany 1 Role: ${company1Role}\nCompany 2 Name: ${company2name}\nCompany 2 Role:${company2role}\nCompany 1 Logo: ${company1logo}\nCompany 2 Logo: ${company2logo}"`
+        `Photo URL: ${photo}\nCompany 1 Name: ${company1Name}\nCompany 1 Role: ${company1Role}\nCompany 2 Name: ${company2name}\nCompany 2 Role: ${company2role}\nCompany 1 Logo: ${company1logo}\nCompany 2 Logo: ${company2logo}"`
       );
       console.log(arr);
       return arr;
     });
-    console.log({ [Object.keys(studentLinks)[200]]: result });
+    console.log({ [Object.keys(studentLinks)[i]]: result });
   }
   // loop ends
 
-  // totalArr.push(result);
-  // let flatArr = totalArr.flat();
-  // console.log(util.inspect(flatArr, { maxArrayLength: null }));
+  totalArr.push(result);
+  let flatArr = totalArr.flat();
+  console.log(util.inspect(flatArr, { maxArrayLength: null }));
 
-  // data.push(totalArr.flat());
   // console.log(totalArr.flat());
 }
 // playtest ends
