b8851b175dd452d850bad41a6efd53c7ecda82a4
michaelscoggins <rhythmghosts@gmail.com>
2021-04-30 00:38:12 -0500

    added some readability (or tried)

--- testScript.js
+++ testScript.js
@@ -3,31 +3,31 @@ const bootcampInfoString = require("./scrapedBootcamps");
 console.log(
   Object.values(bootcampInfoString)
     .join(",")
-    .split("\n")
+    .split("\n") // returns an array of comma-separated-values
     .reduce((obj, cv, idx) => {
-      const currentValueArray = cv.split(",");
+      const currentValueArray = cv.split(","); // splits those values into a more specific array on each iteration
+      const schoolName = currentValueArray[0]; // represents the "school name" value
+      const datapointShape = {
+        company: String(currentValueArray.slice(3)),
+        grads: Number(currentValueArray.slice(2, 3)),
+        state: String(currentValueArray.slice(1, 2)),
+      }; // kinda self-explantory
 
-      if (obj[currentValueArray[0]]) {
+      // if the school name repeats itself, just append that datapoint to the accumulator (obj)
+      if (obj[schoolName]) {
         return (
-          (obj[currentValueArray[0]] = {
-            ...obj[currentValueArray[0]],
-            [idx]: {
-              company: String(currentValueArray.slice(3)),
-              grads: Number(currentValueArray.slice(2, 3)),
-              state: String(currentValueArray.slice(1, 2)),
-            },
+          (obj[schoolName] = {
+            ...obj[schoolName],
+            [idx]: datapointShape,
           }),
           obj
         );
       }
 
+      // if school name doesn't repeat, this condition returns first (creates initial schoolName object/datapoint entry)
       return (
-        (obj[currentValueArray[0]] = {
-          [idx]: {
-            company: String(currentValueArray.slice(3)),
-            grads: Number(currentValueArray.slice(2, 3)),
-            state: String(currentValueArray.slice(1, 2)),
-          },
+        (obj[schoolName] = {
+          [idx]: datapointShape,
         }),
         obj
       );
