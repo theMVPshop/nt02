ae69369c5c84c2478668067ff0649a0f27988c8b
michaelscoggins <rhythmghosts@gmail.com>
2021-04-30 18:15:17 -0500

    some tweaks

--- scrape_scripts/terseCompanyGradsScript.js
+++ scrape_scripts/terseCompanyGradsScript.js
@@ -5,12 +5,13 @@ console.log(
     .split("\n") // returns an array of comma-separated-values with the shape 'School,State,Grads,Company' e.g. 'Dev Bootcamp,CA,14,Amazon',
     .reduce(
       (obj, cv, idx) => (
+        // assembles an object one datapoint at a time with the following shape, and uses the schoolNames as the parent objects
         (obj[cv.split(",")[0]] = {
           ...obj[cv.split(",")[0]],
           [idx]: {
-            company: String(cv.split(",").slice(3)),
-            grads: Number(cv.split(",").slice(2, 3)),
-            state: String(cv.split(",").slice(1, 2)),
+            company: cv.split(",")[3],
+            grads: parseInt(cv.split(",")[2]),
+            state: cv.split(",")[1],
           },
         }),
         obj
