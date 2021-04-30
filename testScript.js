d747f71b6cfc2dba98f4b72d839ebd1eb681f990
michaelscoggins <rhythmghosts@gmail.com>
2021-04-29 22:52:25 -0500

    got it

--- testScript.js
+++ testScript.js
@@ -1,11 +1,32 @@
 const bootcampInfoString = require("./scrapedBootcamps");
-// console.log(bootcampInfoString);
+
 console.log(
   Object.values(bootcampInfoString)
     .join(",")
     .split("\n")
-    .reduce(
-      (obj, cv, idx) => ((obj[cv.split(",")[0]] = cv.split(",").slice(1)), obj),
-      {}
-    )
+    .reduce((obj, cv, idx) => {
+      if (obj[cv.split(",")[0]]) {
+        return (
+          (obj[cv.split(",")[0]] = {
+            ...obj[cv.split(",")[0]],
+            [idx]: {
+              state: String(cv.split(",").slice(1, 2)),
+              grads: String(cv.split(",").slice(2, 3)),
+              company: String(cv.split(",").slice(3)),
+            },
+          }),
+          obj
+        );
+      }
+      return (
+        (obj[cv.split(",")[0]] = {
+          [idx]: {
+            state: String(cv.split(",").slice(1, 2)),
+            grads: String(cv.split(",").slice(2, 3)),
+            company: String(cv.split(",").slice(3)),
+          },
+        }),
+        obj
+      );
+    }, {})
 );
