5464cc9ff707a759f203f760f3ac40b880c8a014
michaelscoggins <rhythmghosts@gmail.com>
2021-05-01 07:12:09 -0500

    states do be different colors tho

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// const stateCounts = require("./data(json)/stateCounts.json");
-document.write("hello world");
-
 $("path, circle").click(function () {
   var getId = this.id;
   var selection = $("#selected-state");
@@ -13,20 +10,44 @@ $("path, circle").click(function () {
   );
 });
 
-// const len = Object.keys(stateCounts).length;
+const stateCounts = {
+  AL: 14,
+  AZ: 18,
+  CA: 157,
+  CO: 78,
+  FL: 24,
+  GA: 15,
+  ID: 12,
+  IL: 41,
+  IN: 28,
+  KY: 24,
+  MA: 8,
+  MI: 14,
+  MN: 14,
+  MO: 13,
+  NC: 25,
+  ND: 12,
+  NH: 6,
+  NJ: 6,
+  NY: 152,
+  OH: 42,
+  OR: 40,
+  PA: 10,
+  TN: 14,
+  TX: 26,
+  UT: 47,
+  VT: 11,
+  WA: 35,
+  WY: 9,
+};
 
-// for (i = 0; i <= len; i++) {
-//   stateKeys = Object.keys(stateCounts);
-//   stateAmounts = Object.values(stateCounts);
-//   let stateIds = document.querySelectorAll(`#${stateKeys[i]}`);
-//   console.log(Object.keys(stateCounts)[i]);
-//   console.log(stateIds);
-//   // if (stateAmounts[i] < 10) stateIds[i].style.color = red;
-//   if (stateAmounts[i] < 10) stateIds[i].fill = "red";
-// }
+for (const property in stateCounts) {
+  let currentStateNode = document.getElementById(property);
+  let currentStateCount = stateCounts[property];
 
-// document.querySelectorAll("#AK").style.color = "red";
-document.querySelectorAll("#AK").fill = "red";
-// document.getElementById("AK").fill = "red";
-// document.getElementById("AK").style.backgroundColor = "red";
-// document.getElementById("AK").style.color = "red";
+  if (currentStateCount < 10) currentStateNode.attributes[1].nodeValue = "blue";
+  if (currentStateCount > 10 && currentStateCount < 25)
+    currentStateNode.attributes[1].nodeValue = "orange";
+  if (currentStateCount > 25)
+    currentStateNode.attributes[1].nodeValue = "green";
+}
--- scrape_scripts/stateCounterScript.js
+++ scrape_scripts/stateCounterScript.js
@@ -3,9 +3,10 @@ console.log(
   Object.values(companyGrads) //
     .join(",") // returns a template literal that basically mirrors the source file
     .split("\n") // returns an array of comma-separated-values with the shape 'School,State,Grads,Company' e.g. 'Dev Bootcamp,CA,14,Amazon',
-    .reduce((obj, cv, idx) => {
+    .reduce((obj, cv) => {
       const currentValueArray = cv.split(",");
       const state = currentValueArray[1];
+
       return (obj[state] = 1 + (obj[state] || 0)), obj;
     }, {})
 );
