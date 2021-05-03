72884c837f223323f4754ab49d3be8ea71186d3e
michaelscoggins <rhythmghosts@gmail.com>
2021-05-03 04:01:38 -0500

    tweaked the 'selected state' function (and removed jquery

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// jquery for showing selected state in header
-$("path, circle").click(function () {
-  var getId = this.id;
-  var selection = $("#selected-state");
-
-  $.getJSON(
-    "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json",
-    function (data) {
-      selection.html(selection).html(data[getId]);
-    }
-  );
-});
-
 // object of how many times each state occurs in company hiring
 const stateCounts = {
   AL: 14,
@@ -42,11 +29,23 @@ const stateCounts = {
   WA: 35,
   WY: 9,
 };
-
-// loops thru stateCounts and assigns color to each state node on the map based on stateCount range
+// loops thru stateCounts and assigns color to each state node on the map based on stateCount range, as well as an eventListener for each state to display as a header onClick
 for (const state in stateCounts) {
-  let stateEl = document.getElementById(state);
-  let stateCount = stateCounts[state];
+  const stateEl = document.getElementById(state);
+  let selectedStateTitleEl = document.getElementById("selected-state");
+  const stateCount = stateCounts[state];
+
+  stateEl.addEventListener("click", () => {
+    fetch(
+      "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        selectedStateTitleEl.innerHTML = `{<span style="color:blue">${data[state]}</span>:
+          <span style="color:green">${stateCount}</span>
+         bootcamp grads hired}`;
+      });
+  });
 
   if (stateCount < 15) stateEl.attributes[1].nodeValue = "rgb(0,100,0, .32)";
   if (stateCount >= 15 && stateCount <= 35)
